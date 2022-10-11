689c9edefd8ec58d82158fa2184ac39d365b54fd
shadman-shahid <shadman9085@gmail.com>
2022-10-12 02:03:45 +0600

    Updated the fem file.

--- fem_test_2.m
+++ fem_test_2.m
@@ -1,15 +1,31 @@
 % clear all;
 % pdetool;
-boundary_value_1 = 0;
-eps = [200, 8];
-% boundary = unique([e(1,e(5,:)==13),e(2,e(5,:)==13)]);
+%% Load the geometry of our desired structure
+load geometry_description.mat
 
+%% Get [p,e,t] mesh data
+d = decsg(gd, sf, ns);
+figure
+pdegplot(d,'EdgeLabels','on','SubdomainLabels','on')
+axis equal
+figure
+[p,e,t] = initmesh(d);
+pdemesh(p,e,t,'NodeLabels','on')
+
+boundary_value_1 = 1;
+
+% Defining the boundaries and known node values. (Dirichelet Condition)
+dirichelet_elem = t(4,:)==1;
+metal_nodes = unique(t(1:3, dirichelet_elem));
+
+% boundary = unique([e(1,e(5,:)==13),e(2,e(5,:)==13)]);
 dom_bound = e(6:7,:)~=[1;2] & e(6:7,:)~=[2;1];
 dom_bound = dom_bound(1,:) | dom_bound(2,:);
-dom_bound = dom_bound & e(6,:)~=0;
+% dom_bound = dom_bound & e(6,:)~=0;
 
-outside_b = unique([e(1,dom_bound),e(2,dom_bound)]);
-inside_b = unique([e(1,e(6,:)==0),e(2,e(6,:)==0)]);
+inside_b = unique([e(1,e(6,:)~=0 & dom_bound),e(2,e(6,:)~=0 & dom_bound)]);
+outside_b = unique([e(1,e(6,:)==0),e(2,e(6,:)==0)]);
+% domain_b
 % ib = unique(e(5,e(5,:)==1));
 
 n_dom = max(unique(t(4,:)));
@@ -20,13 +36,19 @@ ip = size(e,2);
 a= zeros(ne,1);
 A = zeros(np);
 b = zeros(np,1);
+eps = ones(ne, 2);
+phi = zeros(np,1);
 
 inside_boundary = zeros(np, 1);
 inside_boundary(inside_b) = 1;
 inside_boundary = logical(inside_boundary);
+
+eps(t(4,:) == 1,:) = 122.9;
+eps(t(4,:) == 2,:) = 1;
+
 for sub_domain = 1:n_dom
     [~,id] = find(t(4,:)==sub_domain);
-    p_sub_dom = unique(t(1:3,id));
+%     p_sub_dom = unique(t(1:3,id));
     for k = 1:length(id)
         i1 = t(1, id(k));
         i2 = t(2, id(k));
@@ -45,15 +67,15 @@ for sub_domain = 1:n_dom
 
         g = [g1, g2, g3];
 
-        A(i1, i1) = A(i1, i1) + a(id(k)) * dot(g1, g1);
-        A(i2, i2) = A(i2, i2) + a(id(k)) * dot(g2, g2);
-        A(i3, i3) = A(i3, i3) + a(id(k)) * dot(g3, g3);
-        A(i1, i2) = A(i1, i2) + a(id(k)) * dot(g1, g2);
-        A(i1, i3) = A(i1, i3) + a(id(k)) * dot(g1, g3);
-        A(i2, i1) = A(i2, i1) + a(id(k)) * dot(g2, g1);
-        A(i2, i3) = A(i2, i3) + a(id(k)) * dot(g2, g3);
-        A(i3, i1) = A(i3, i1) + a(id(k)) * dot(g3, g1);
-        A(i3, i2) = A(i3, i2) + a(id(k)) * dot(g3, g2);
+        A(i1, i1) = A(i1, i1) + a(id(k)) * dot(eps(k,:)'.*g1, g1);
+        A(i2, i2) = A(i2, i2) + a(id(k)) * dot(eps(k,:)'.*g2, g2);
+        A(i3, i3) = A(i3, i3) + a(id(k)) * dot(eps(k,:)'.*g3, g3);
+        A(i1, i2) = A(i1, i2) + a(id(k)) * dot(eps(k,:)'.*g1, g2);
+        A(i1, i3) = A(i1, i3) + a(id(k)) * dot(eps(k,:)'.*g1, g3);
+        A(i2, i1) = A(i2, i1) + a(id(k)) * dot(eps(k,:)'.*g2, g1);
+        A(i2, i3) = A(i2, i3) + a(id(k)) * dot(eps(k,:)'.*g2, g3);
+        A(i3, i1) = A(i3, i1) + a(id(k)) * dot(eps(k,:)'.*g3, g1);
+        A(i3, i2) = A(i3, i2) + a(id(k)) * dot(eps(k,:)'.*g3, g2);
         % imposing non-zero dirichelet condition on A matrix
     %     A(inside_b, :) = 0;
     %     A(:, inside_b) = 0;
@@ -69,10 +91,11 @@ for sub_domain = 1:n_dom
         b(i3) = b(i3) + a(id(k)) ./ 3;
 
     end
-    A(p_sub_dom, p_sub_dom) =  A(p_sub_dom, p_sub_dom).* eps(sub_domain);
+%     A(p_sub_dom, p_sub_dom) =  A(p_sub_dom, p_sub_dom).* eps(sub_domain);
 end
 
 interior = ones(np,1);
+% interior(dom_bound) = 0;
 interior(outside_b) = 0;
 interior(inside_b) = 0;
 % interior(boundary) = 0;
